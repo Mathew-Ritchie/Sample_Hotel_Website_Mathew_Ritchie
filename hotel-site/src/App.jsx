94e5b3fca98bae8ac5de0ff4e5d84a89c787f48a
Mathew Ritchie <ritchmat89@gmail.com>
2026-01-07 13:37:29 +0000

    initial commit.

---
+++ hotel-site/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1 className='text-red-500 font-bold text-6xl'>Hello</h1>
+      </div>
+  )
+}
+
+export default App
